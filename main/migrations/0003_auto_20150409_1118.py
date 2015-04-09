4c3ae6bbad7e1e9e5ebec60b0f7e6aa6317d38c5
cheney93 <823506023@qq.com>
2015-04-09 16:20:59 +0800

    优化api

---
+++ main/migrations/0003_auto_20150409_1118.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0002_comment'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(related_name='comments', to='main.Post'),
+            preserve_default=True,
+        ),
+    ]
